0dd22c7b3eaae0a75ed02828179bd796ac6052be
Maciej Plonski <info@sokoli.pl>
2025-10-02 23:51:21 +0200

    remove grafana dashboard

--- prometheus_exporter/http.go
+++ prometheus_exporter/http.go
@@ -10,12 +10,6 @@ func RunHTTPServer(logger *slog.Logger) {
 	go CollectSmartCtlStats(logger)
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.HandleFunc("/dashboard.json", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "/monitoring/dashboard.json")
-	})
-	http.HandleFunc("/prometheusRule.yml", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "/monitoring/prometheusRule.yml")
-	})
 
 	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
